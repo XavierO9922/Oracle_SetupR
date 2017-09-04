8033b0745adc38d4820fe3b3aa19532bb227e40e
Livier2492 <livier.hernandez@oracle.com>
2017-09-04 17:14:38 -0500

    Add basic table to home page

--- public_html/js/viewModels/home.js
+++ public_html/js/viewModels/home.js
@@ -7,13 +7,25 @@
 /**
  * home module
  */
-define(['ojs/ojcore', 'knockout'
-], function (oj, ko) {
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'promise', 
+    'ojs/ojtable', 'ojs/ojarraytabledatasource'
+], function (oj, ko, $) {
     /**
      * The view model for the main content view template
      */
     function homeContentViewModel() {
         var self = this;
+        
+        var deptArray = [{SCMModule: 'Product Development', RequestType: 'EFF Attribute setup', RequestDetails: 'Setup EFF on Item Class Hardware', Requestor: 'xxx@oracle.com', Status: 'Awaiting Approval'},
+        {SCMModule: 'Order Management', RequestType: 'EFF Attribute setup', RequestDetails: 'Setup DFF on Item Class Hardware', Requestor: 'xxx@oracle.com', Status: 'Awaiting Approval'},
+        {SCMModule: 'Order Management', RequestType: 'EFF Attribute setup', RequestDetails: 'Setup new Item Class', Requestor: 'xxx@oracle.com', Status: 'Awaiting Approval'},
+        {SCMModule: 'Costing', RequestType: 'EFF Attribute setup', RequestDetails: 'Setup EFF on Item Class', Requestor: 'xxx@oracle.com', Status: 'Awaiting Approval'},
+        {SCMModule: 'Costing', RequestType: 'EFF Attribute setup', RequestDetails: 'Setup EFF on Item Class', Requestor: 'xxx@oracle.com', Status: 'Awaiting Approval'},
+        {SCMModule: 'Product Development', RequestType: 'EFF Attribute setup', RequestDetails: 'Setup DFF on Item Class Phantom', Requestor: 'xxx@oracle.com', Status: 'Awaiting Approval'},
+        {SCMModule: 'Product Development', RequestType: 'EFF Attribute setup', RequestDetails: 'Setup EFF on Item Class Phantom', Requestor: 'xxx@oracle.com', Status: 'Awaiting Approval'},
+        {SCMModule: 'Product Development', RequestType: 'EFF Attribute setup', RequestDetails: 'Setup EFF on Item Class Phantom', Requestor: 'xxx@oracle.com', Status: 'Awaiting Approval'}];
+    
+        self.datasource = new oj.ArrayTableDataSource(deptArray, {idAttribute: 'SCMModule'});
     }
     
     return homeContentViewModel;
